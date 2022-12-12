4a7b067dd4a10a689d8bfdc29800780c7eb39ca9
Stanley Ayo <hello@scaleitpro.com>
2022-12-12 18:52:23 +0100

    print small and capital letters using for loop

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints the alphabet in lowercase, and then in uppercase.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-        putchar(alphabet);
-}
-for (alphabet ='A'; alphabet <= 'Z'; alphabet++)
-{
-	putchar(alphabet);
-}
-        putchar('\n');
-return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
