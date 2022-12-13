4ee948fd3bf50e4e580258ce198e36c649b1cdca
Stanley Ayo <hello@scaleitpro.com>
2022-12-13 18:22:13 +0100

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Author - Stanley Ayo
+ *
+ * main begins code
+ *
+ * return value is zero
+ */
+
+int main(void){
+    printf("_putchar\n");
+    return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ALX Project tasks on Functions and Nested Loops
