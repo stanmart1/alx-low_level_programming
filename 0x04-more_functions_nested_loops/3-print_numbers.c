69da80c8cdeb7df438db3ae183a9b3ec6ad7b5ea
Stanley Ayo <stanleym37@gmail.com>
2022-12-15 13:26:16 -0800

    print numbers using _putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
-  * print_numbers - Print the numbers from 0 up to 9
-  *
-  * Return: The numbers from 0 up to 9
-  */
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9 followed by newline
+ */
 void print_numbers(void)
 {
-	int x;
+	int i;
 
-	for (x = 0; x <= 9; x++)
-	{
-		_putchar(x + '0');
-	}
-
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
 	_putchar('\n');
-
+}
