0c4bf7c7fac9547b5be8decd92dd8f9255a219e0
Stanley Ayo <stanleym37@gmail.com>
2022-12-19 16:56:19 -0800

    function that prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * _strcmp - a function that compares two strings
- * @s1: input one
- * @s2: input two
- * Return: Always 0 (Success)
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-	}
-	return (0);
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+void print_string(const char* str) {
+    while (*str != '\0') {
+        putchar(*str);
+        str++;
+    }
+    putchar('\n');
 }
