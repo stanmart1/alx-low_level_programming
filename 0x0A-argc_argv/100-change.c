ad71492c17b5016f0582a0766a0286c58ee7aab9
Stanley Ayo <stanleym37@gmail.com>
2023-01-06 03:53:13 -0800

    program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,47 +10,44 @@
   * Return: Always zero
   */
 int main(int argc, char *argv[]) {
-// Check the number of arguments passed
-if (argc != 2) {
-printf("Error\n");
-return 1;
-}
+  // Check number of arguments
+  if (argc != 2) {
+    printf("Error\n");
+    return 1;
+  }
 
-// Convert the string argument to an integer
-int cents = atoi(argv[1]);
+  // Parse argument as integer
+  int cents = atoi(argv[1]);
 
-// Check if the amount is negative
-if (cents < 0) {
-printf("0\n");
-return 0;
-}
+  // Check if argument is negative
+  if (cents < 0) {
+    printf("0\n");
+    return 0;
+  }
 
-// Initialize the count of coins to 0
-int coin_count = 0;
+  // Initialize counter for number of coins
+  int numCoins = 0;
 
-// While there are still cents to be returned,
-// use the largest coin possible
-while (cents > 0) {
-if (cents >= 25) {
-cents -= 25;
-coin_count++;
-} else if (cents >= 10) {
-cents -= 10;
-coin_count++;
-} else if (cents >= 5) {
-cents -= 5;
-coin_count++;
-} else if (cents >= 2) {
-cents -= 2;
-coin_count++;
-} else {
-cents -= 1;
-coin_count++;
-}
-}
+  // Use as many 25 cent coins as possible
+  numCoins += cents / 25;
+  cents %= 25;
 
-// Print the minimum number of coins
-printf("%d\n", coin_count);
+  // Use as many 10 cent coins as possible
+  numCoins += cents / 10;
+  cents %= 10;
 
-return 0;
+  // Use as many 5 cent coins as possible
+  numCoins += cents / 5;
+  cents %= 5;
+
+  // Use as many 2 cent coins as possible
+  numCoins += cents / 2;
+  cents %= 2;
+
+  // Use as many 1 cent coins as possible
+  numCoins += cents;
+
+  // Print number of coins
+  printf("%d\n", numCoins);
+  return 0;
 }
