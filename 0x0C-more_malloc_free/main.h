5f041ff93ca88f507644eb1edcda125cbed12d7b
Stanley Ayo <stanleym37@gmail.com>
2023-01-12 08:10:47 -0800

    function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
 #endif /* MAIN_H */
